50f2b9deb72cb46d068846b84e65fc021585579b
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2020-03-21 10:30:29 -0700

    Implement basic lyric parsing.

--- Songs2Slides.py
+++ Songs2Slides.py
@@ -11,3 +11,20 @@ def getLyrics(artist, song):
     html = BeautifulSoup(page.text, 'html.parser')
     lyrics = html.find('div', class_='lyrics').get_text()
     return lyrics
+
+
+# Parses the lyrics into blocks
+def ParseLyrics(lyrics):
+    rawLines = lyrics.split("\n")
+    lines = []
+    BlockSize = 0
+    for i in range(0, len(rawLines)):
+        if (rawLines[i] == "" or rawLines[i][0] == "["):
+            BlockSize = 4
+        elif (BlockSize == 4):
+            lines.append(rawLines[i])
+            BlockSize = 1
+        else:
+            lines[-1] = lines[-1] + "\n" + rawLines[i]
+            BlockSize += 1
+    return lines
